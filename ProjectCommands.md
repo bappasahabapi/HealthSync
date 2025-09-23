e5da32f5f729121c57f02cb3a521057c116f2048
bappasaha <bappabappa840@gmail.com>
2025-09-24 01:47:43 +0600

    fix(status-code): http-status-codes for avoid hardcoding status

--- ProjectCommands.md
+++ ProjectCommands.md
@@ -57,3 +57,4 @@ After change any model run the migration command
 ---
 
 - 1: Response Formatting and Exception HandlingRes
+    - `npm install http-status-codes` for avoid hardcoding
--- src/app/modules/User/user.controllers.ts
+++ src/app/modules/User/user.controllers.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express";
 import { userService } from "./user.services";
+import { StatusCodes } from "http-status-codes";
 
 const createAdmin = async (req: Request, res: Response) => {
   try {
     const result = await userService.createAdmin(req.body);
     // res.send(result);
-    res.status(200).json({
-        success:true,
-        message:" ✅ Admin Created Successfully",
-        data:result
-    })
+
+     res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "✅ Admin Created Successfully",
+      data: result,
+    });
   } catch (error:any) {
-    res.status(500).json({
+    // res.status(500).json({
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({    
         success:false,
         message:error?.name || " ❌ Something went wrong",
         error:error
