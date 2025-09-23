161d1503ad6530b12848ab4e62bebcc486e09ab7
bappasaha <bappabappa840@gmail.com>
2025-09-24 03:27:50 +0600

    feat(admin): optimized if no searchTerm

--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -1,36 +1,78 @@
-import { PrismaClient } from "../../../generated/prisma";
+// import { PrismaClient } from "../../../generated/prisma";
+
+// const prisma = new PrismaClient();
+
+// const getAdmins = async (params: any) => {
+//   console.log({ params });
+//   const result = await prisma.admin.findMany({
+
+//     //search
+//     where: {
+//       OR: [
+//         {
+//           name: {
+//             contains: params.searchTerm,
+//             mode: "insensitive", // optional, case-insensitive search
+//           },
+//         },
+//         {
+//              email:{
+//             contains: params.searchTerm,
+//             mode: "insensitive",
+//           }
+//         },
+//         {
+//              contactNumber:{
+//             contains: params.searchTerm,
+//             mode: "insensitive",
+//           }
+//         }
+//       ],
+//     },
+//   });
+
+//   return result;
+// };
+
+// export const adminService = {
+//   getAdmins,
+// };
+
+
+//todo: another way to handle if no searchTerm
+import { PrismaClient, Prisma } from "../../../generated/prisma";
 
 const prisma = new PrismaClient();
 
 const getAdmins = async (params: any) => {
-  console.log({ params });
-  const result = await prisma.admin.findMany({
+  const searchTerm = params?.searchTerm as string | undefined;
 
-    //search
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive", // optional, case-insensitive search
+  const where: Prisma.AdminWhereInput = searchTerm
+    ? {
+        OR: [
+          {
+            name: {
+              contains: searchTerm,
+              mode: "insensitive" as Prisma.QueryMode,
+            },
           },
-        },
-        {
-             email:{
-            contains: params.searchTerm,
-            mode: "insensitive", 
-          }
-        },
-        {
-             contactNumber:{
-            contains: params.searchTerm,
-            mode: "insensitive", 
-          }
-        }
-      ],
-    },
-  });
-  return result;
+          {
+            email: {
+              contains: searchTerm,
+              mode: "insensitive" as Prisma.QueryMode,
+            },
+          },
+          {
+            contactNumber: {
+              contains: searchTerm,
+              mode: "insensitive" as Prisma.QueryMode,
+            },
+          },
+        ],
+      }
+    : {}; // no filter if no searchTerm
+
+  return prisma.admin.findMany({ where });
 };
 
 export const adminService = {
