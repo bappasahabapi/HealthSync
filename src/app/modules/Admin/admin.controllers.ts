1d94227ed37bd1cd0bc147decab2ceebd5108260
bappasaha <bappabappa840@gmail.com>
2025-09-24 17:07:59 +0600

    feat(admin): add validation for exact search fild using pick function

--- src/app/modules/Admin/admin.controllers.ts
+++ src/app/modules/Admin/admin.controllers.ts
@@ -2,21 +2,38 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { adminService } from "./admin.services";
 
+const pick = (obj:any, keys:any) => {
+  // console.log(obj,keys)
+
+  const finalObject:any ={};
+
+  for(const key of keys){
+    if(obj && Object.hasOwnProperty.call(obj,key)){
+      // console.log(key)
+      finalObject[key] =obj[key]
+    }
+  }
+  // console.log(finalObject)
+  return finalObject;
+};
+
 const getAdmins = async (req: Request, res: Response) => {
+
   try {
-    const result = await adminService.getAdmins(req.query);
+    // pick(req.query,keyName)
+    const filters = pick(req.query,['name','email','contactNumber','searchTerm'])
+    const result = await adminService.getAdmins(filters);
     res.status(StatusCodes.OK).json({
       success: true,
       message: "✅ Admin data fetched Successfully",
       data: result,
     });
-  } catch (err:any) {
-     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({    
-        success:false,
-        message:err?.message || " ❌ Something went wrong",
-        error:err
-
-    })
+  } catch (err: any) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err?.message || " ❌ Something went wrong",
+      error: err,
+    });
   }
 };
 
