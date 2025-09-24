e9ed39a6269f48281d36af46f452758196e58999
bappasaha <bappabappa840@gmail.com>
2025-09-24 17:21:37 +0600

    refactor(admin): add pic function for shared used

--- src/app/modules/Admin/admin.controllers.ts
+++ src/app/modules/Admin/admin.controllers.ts
@@ -1,21 +1,8 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { adminService } from "./admin.services";
+import pick from "../../../shared/pick";
 
-const pick =<T extends Record<string, unknown>, k extends keyof T> (obj:T, keys:k[]):Partial<T> => {
-  // console.log(obj,keys)
-
-  const finalObject: Partial<T> ={};
-
-  for(const key of keys){
-    if(obj && Object.hasOwnProperty.call(obj,key)){
-      // console.log(key)
-      finalObject[key] =obj[key]
-    }
-  }
-  // console.log(finalObject)
-  return finalObject;
-};
 
 const getAdmins = async (req: Request, res: Response) => {
 
