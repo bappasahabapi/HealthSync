a50bcb7d28331d2b48c45a1c9f73e5f0bb92c044
bappasaha <bappabappa840@gmail.com>
2025-09-24 14:22:52 +0600

    bug(fix): Fixed wrong import in prisma

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,4 +1,4 @@
-import { PrismaClient,UserRole  } from "@prisma/client" 
+import { PrismaClient, UserRole } from "@prisma/client";
 import * as bcrypt from "bcrypt";
 
 const prisma =new PrismaClient();
